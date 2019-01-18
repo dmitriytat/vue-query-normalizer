d421c682a4041cc0bbf2f280dfff9745e1614376
Dmitriy <tatarintsevd@gmail.com>
2019-01-18 13:12:11 +0300

    fix: export in d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,7 @@
 import Vue, {ComponentOptions, PluginObject} from "vue/types/index";
 
 declare const queryNormalizerMixin: ComponentOptions<Vue>;
-
 declare const QueryNormalizer: PluginObject<Vue>;
+
+export default QueryNormalizer;
+export queryNormalizerMixin;
